385e6fa50cba1a11af13a091f7ad6d6dbf0e18f5
Matheus Avi <26263503+matheusavi@users.noreply.github.com>
2025-03-18 22:57:17 -0300

    Trying to add an effective compact number parsing

--- src/block.rs
+++ src/block.rs
@@ -1,5 +1,6 @@
 use hex::decode;
 use sha256::digest;
+use std::f64;
 
 pub struct Block {
     pub version: i32, // not sure if I should already store this as little endian constants :crazy:
@@ -58,6 +59,46 @@ impl Block {
 mod tests {
     use super::*;
     use hex::decode;
+    use primitive_types::U256;
+
+    #[test]
+    fn mine() {
+        let mut block = Block {
+            version: 1,
+            previous_block_hash: String::from(
+                "0000000000000000000000000000000000000000000000000000000000000000",
+            ),
+            merkle_root_hash: String::from(
+                "3ba3edfd7a7b12b27ac72c3e67768f617fc81bc3888a51323a9fb8aa4b1e5e4a",
+            ),
+            time: 0x495fab29,
+            n_bits: 0xffff001d,
+            nonce: 0x7c2bac1d,
+            hash: String::new(),
+            mine_array: [0; 80],
+        };
+
+        block.prepare_for_mining();
+
+        //I'm pretty sure this is wrong
+        let hash = block.get_hash();
+
+        let hash = U256::from_big_endian(&hash);
+        let target: u32 = 0xffff001d;
+        let exponent = target >> 24;
+        let mantissa = target & 0x007FFFFF;
+        let target = if exponent <= 3 {
+            U256::from(mantissa >> (8 * (3 - exponent)))
+        } else {
+            let shift = 8 * (exponent - 3);
+            if shift >= 256 {
+                U256::max_value()
+            } else {
+                U256::from(mantissa) << shift
+            }
+        };
+        assert!(hash < target);
+    }
 
     #[test]
     fn test_pre_hash() {
