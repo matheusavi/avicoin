c7bb0a76ff5d2c68036019c41c5c8bca83132b6b
matheusavi <26263503+matheusavi@users.noreply.github.com>
2025-05-22 22:12:17 -0300

    Add reader tests

--- src/byte_reader.rs
+++ src/byte_reader.rs
@@ -4,11 +4,11 @@ pub struct ByteReader<'a> {
 }
 
 impl<'a> ByteReader<'a> {
-    fn new(bytes: &'a [u8]) -> Self {
+    pub fn new(bytes: &'a [u8]) -> Self {
         Self { bytes, position: 0 }
     }
-    fn read_byte(&mut self) -> Result<u8, String> {
-        if self.position + 1 >= self.bytes.len() {
+    pub fn read_byte(&mut self) -> Result<u8, String> {
+        if self.position >= self.bytes.len() {
             return Err(String::from("EOF"));
         }
         let byte = self.bytes[self.position];
@@ -16,8 +16,8 @@ impl<'a> ByteReader<'a> {
         Ok(byte)
     }
 
-    fn read_u16(&mut self) -> Result<u16, String> {
-        if self.position + 2 >= self.bytes.len() {
+    pub fn read_u16(&mut self) -> Result<u16, String> {
+        if self.position + 2 > self.bytes.len() {
             return Err(String::from("EOF"));
         }
         let byte = u16::from_le_bytes(
@@ -29,8 +29,8 @@ impl<'a> ByteReader<'a> {
         Ok(byte)
     }
 
-    fn read_u32(&mut self) -> Result<u32, String> {
-        if self.position + 4 >= self.bytes.len() {
+    pub fn read_u32(&mut self) -> Result<u32, String> {
+        if self.position + 4 > self.bytes.len() {
             return Err(String::from("EOF"));
         }
         let byte = u32::from_le_bytes(
@@ -41,5 +41,36 @@ impl<'a> ByteReader<'a> {
         self.position += 4;
         Ok(byte)
     }
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_byte_empty() {
+        let bytes = [];
+        let mut reader = ByteReader::new(&bytes);
+        let result = reader.read_byte();
+        assert!(result.is_err());
+        assert_eq!(result.unwrap_err(), "EOF");
+    }
+
+    #[test]
+    fn test_mixed_type_reads() {
+        let bytes = [42, 1, 2, 3, 4, 5, 6, 7, 8];
+        let mut reader = ByteReader::new(&bytes);
+
+        assert_eq!(reader.read_byte().unwrap(), 42);
+
+        assert_eq!(reader.read_u32().unwrap(), 0x04030201);
+
+        assert_eq!(reader.read_u16().unwrap(), 0x0605);
+
+        assert_eq!(reader.read_byte().unwrap(), 7);
+
+        assert_eq!(reader.read_byte().unwrap(), 8);
+
+        assert!(reader.read_byte().is_err());
+    }
 }
