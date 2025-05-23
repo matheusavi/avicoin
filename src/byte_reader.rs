fd0c44c8cefb667f150abf8b57208cbede8dfff8
matheusavi <26263503+matheusavi@users.noreply.github.com>
2025-05-22 21:55:56 -0300

    Add more reader methods

--- src/byte_reader.rs
+++ src/byte_reader.rs
@@ -7,21 +7,39 @@ impl<'a> ByteReader<'a> {
     fn new(bytes: &'a [u8]) -> Self {
         Self { bytes, position: 0 }
     }
-
     fn read_byte(&mut self) -> Result<u8, String> {
-        if self.position >= self.bytes.len() {
+        if self.position + 1 >= self.bytes.len() {
             return Err(String::from("EOF"));
         }
         let byte = self.bytes[self.position];
         self.position += 1;
         Ok(byte)
     }
+
     fn read_u16(&mut self) -> Result<u16, String> {
-        if self.position >= self.bytes.len() {
+        if self.position + 2 >= self.bytes.len() {
             return Err(String::from("EOF"));
         }
-        let byte = self.bytes[self.position];
-        self.position += 1;
+        let byte = u16::from_le_bytes(
+            self.bytes[self.position..self.position + 2]
+                .try_into()
+                .map_err(|_| String::from("Invalid u16 bytes"))?,
+        );
+        self.position += 2;
         Ok(byte)
     }
+
+    fn read_u32(&mut self) -> Result<u32, String> {
+        if self.position + 4 >= self.bytes.len() {
+            return Err(String::from("EOF"));
+        }
+        let byte = u32::from_le_bytes(
+            self.bytes[self.position..self.position + 4]
+                .try_into()
+                .map_err(|_| String::from("Invalid u32 bytes"))?,
+        );
+        self.position += 4;
+        Ok(byte)
+    }
+
 }
