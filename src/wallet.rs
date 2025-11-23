68060baf88a19690daf39061ed2727fbc5bd0906
matheusavi <26263503+matheusavi@users.noreply.github.com>
2025-11-23 15:33:49 -0300

    Send method

--- src/wallet.rs
+++ src/wallet.rs
@@ -1,9 +1,9 @@
-use std::io::ErrorKind;
+use crate::transaction::{Transaction, TxIn, TxOut};
 use anyhow::anyhow;
 use secp256k1::hashes::{sha256, Hash};
 use secp256k1::{rand, PublicKey, SecretKey};
 use secp256k1::{Message, Secp256k1};
-use crate::transaction::{Transaction, TxIn};
+use std::io::ErrorKind;
 
 #[derive(Clone, Debug)]
 pub struct Wallet {
@@ -26,30 +26,38 @@ impl Wallet {
         // TODO get from UTXO module
         10000000
     }
-    
+
     pub fn send(amount: u64, fee: u64, destination_address: String) -> Result<Transaction> {
         if amount + fee > Self::get_available_balance() {
             return Err(anyhow!("Insufficient funds"));
         }
-        
+
         // get available utxo
         let outpoints = Self::get_outpoints(amount, fee);
-        
+
+        let sig = Secp256k1::signing_only();
+
         let mut inputs = Vec::new();
+
         for outpoint in outpoints {
-            let input = TxIn{
-                previous_output : outpoint,
-                signature: , // sign here
+            inputs.push(TxIn {
+                previous_output: outpoint,
+                signature: sig.sign_ecdsa(outpoint.tx_id, Self.private_key),
                 sequence: 0xFFFFFFFF,
-            }
+            })
         }
-       
+
         // create change
-        
+
         // create a new transaction
-        Transaction{
+        Ok(Transaction {
             version: 1,
-            
-        }
+            inputs,
+            outputs: vec![TxOut {
+                value: amount,
+                destiny_pub_key: destination_address,
+            }],
+            lock_time: 0,
+        })
     }
 }
